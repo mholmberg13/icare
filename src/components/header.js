6bb6a99ed1f821aa4188dc113b0daf04c1c894b3
Matt Holmberg <mholmberg13@gmail.com>
2020-08-21 13:18:41 -0600

    Built header componenet and set up file structure

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>iCare</h1>
+                <h3>Donate. Change Lives.</h3>
+            </div>
+        )
+    }
+}
+
+export default Header
