12efb41f6cd2cb18e827d3a1060387cda5c15082
Matt Holmberg <mholmberg13@gmail.com>
2020-09-09 11:24:49 -0600

    Added Nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav.js'
 
 function App() {
   return (
     <div className="App">
       <h1>iCare</h1>
+      <Nav></Nav>
         <p>
           Welcome to iCare.
         </p>
---
+++ src/components/nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Nav extends React.Component {
+
+    render () {
+        return (
+            <div className="nav-container">
+                <ul className="nav-list">
+                    <li>Home</li>
+                    <li>Charities</li>
+                    <li>Become Certified</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        )
+    }
+
+
+}
+
+export default Nav;
